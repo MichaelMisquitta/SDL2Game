e19fbff3b8c1bb9053c1df6119b0b151a9fdfbae
michael misquitta <michaelmisquitta@gmail.com>
2023-12-19 18:29:20 -0600

    screen color working

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main(int charc,char *argv[]){
 
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window *window = SDL_CreateWindow("test",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,WIDTH,HEIGHT,SDL_WINDOW_ALLOW_HIGHDPI);
-
+    SDL_Window *window = SDL_CreateWindow("test",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window,-1,0);
     if (window == NULL){
         std::cout << "window error"<<SDL_GetError();
         return 1;
@@ -22,8 +22,12 @@ int main(int charc,char *argv[]){
                 break;
             }
         }
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer,100,50,250,255);
+        SDL_RenderPresent(renderer); // call once per frame to put buffer of drawings to screen
     }
     std::cout << "successs bingus 2";
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
