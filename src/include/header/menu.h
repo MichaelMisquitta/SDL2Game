a449f083b7134ca0925a6fec96f23810b1285e29
MichaelMisquitta <michaelmisquitta@gmail.com>
2023-12-26 01:39:48 -0600

    rearranged into two libraries - SDL and SDL_ttf to see if it compiles and it does

--- src/include/header/menu.h
+++ src/include/header/menu.h
@@ -1,14 +1,14 @@
 #ifndef MENU_H
 #define MENU_H
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
+#include <SDL2_ttf/SDL_ttf.h>
 
 class menu{
     public:
     SDL_Renderer* renderer;
     //constructor
     menu(SDL_Renderer*);
-    TTF_Font* font = NULL; //testing out sdl_ttf
+    TTF_Font* font = TTF_OpenFont("Raleway-Heavy.ttf",13); //testing out sdl_ttf
 
     //destructor
     
